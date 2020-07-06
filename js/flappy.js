9b90820fdebe4ed79096a16e170274a5526919df
Glauber Brack <glauber@brack.com.br>
2020-07-05 23:27:49 -0300

    animation barriers

--- js/flappy.js
+++ js/flappy.js
@@ -34,17 +34,51 @@ function DoubleBarriers(height, openspace, position){
         this.bottom.setHeight(bottomHeight)
     }
 
-    this.getX = () => parent(this.element.style.left.split('px'))
+    this.getPosition = () => parseInt(this.element.style.left.split('px')[0])
 
-    this.setX = position => this.element.style.left = `${position}px`
+    this.setPosition = position => this.element.style.left = `${position}px`
 
     this.getWidth = () => this.element.clientWidth
 
     this.randomOpenScace()
-    this.setX(position)
+    this.setPosition(position)
+}
+
+function Barriers(height, width, openspace, space, pointNotification){
+    this.pairs = [
+        new DoubleBarriers(height, openspace, width),
+        new DoubleBarriers(height, openspace, width + space),
+        new DoubleBarriers(height, openspace, width + space * 2),
+        new DoubleBarriers(height, openspace, width + space * 3)
+    ]
+
+    const displacement = 3
+
+    this.animate = () => {
+        this.pairs.forEach(pair => {
+            pair.setPosition(pair.getPosition() - displacement)
+
+            if(pair.getPosition() < pair.getWidth()){
+                pair.setPosition(pair.getPosition() + space * this.pairs.length)
+                pair.randomOpenScace()
+            }
+
+            const middle = width / 2
+            const middleCross = pair.getPosition() + displacement >= middle
+                && pair.getPosition() < middle
+            if(middleCross) pointNotification()
+        })
+    }
 }
 
 
-// const b = new DoubleBarriers(700, 200,800)
-// document.querySelector('[tp-flappy]').appendChild(b.element)
+const barriers = new Barriers(700, 1200, 300, 400)
+
+const gameArea = document.querySelector('[tp-flappy]')
+barriers.pairs.forEach(pair => gameArea.appendChild(pair.element))
+
+setInterval(() => {
+    barriers.animate()
+}, 20)
+
 
